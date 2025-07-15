be3260cc876db894f0e9bde799798d33bf479c7b
Kevin Marr <caseymarr@gmail.com>
2025-07-15 16:37:28 -0700

    fix: Correct logo path for production builds (v2)

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
 	import '@evidence-dev/tailwind/fonts.css';
 	import '../app.css';
+	import { base } from '$app/paths';
 	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
 	export let data;
 </script>
 
-<EvidenceDefaultLayout {data} logo="/logo.png">
+<EvidenceDefaultLayout {data} logo={`${base}/logo.png`}>
 	<slot slot="content" />
 </EvidenceDefaultLayout>
